9a18b4af7f8a351a76c2a30c9699d1131a9947f8
Maksim_Sapunov <Jenny6199@yandex.ru>
2022-04-15 11:29:01 +0300

    prepare model Notebook

--- personal_db/book/admin.py
+++ personal_db/book/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Notebook
 
-# Register your models here.
+
+class NotebookAdmin(admin.ModelAdmin):
+    """
+    Класс для настройки отображения моделей в
+    админ-панели.
+    """
+    list_display = ()
+    list_display_links = ()
+    search_fields = ()
+
+
+admin.site.register(Notebook, NotebookAdmin)
--- personal_db/book/models.py
+++ personal_db/book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Notebook(models.Model):
+    """
+    Модель хранения данных.
+    """
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
