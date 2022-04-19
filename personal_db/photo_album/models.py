f5e7126b216d325714e8001f83c8bc3f17f8e4fc
Maksim_Sapunov <Jenny6199@yandex.ru>
2022-04-19 13:12:10 +0300

    start model Photo

--- personal_db/photo_album/models.py
+++ personal_db/photo_album/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 
+class Photo(models.Model):
+    class Meta:
+        verbose_name = 'фотография'
+        verbose_name_plural = 'фотографии'
+    pass
+
+
 class Album(models.Model):
     class Meta:
         verbose_name = 'альбом'
         verbose_name_plural = 'альбомы'
 
-    pass
+    name = models.CharField(max_length=64, help_text='название альбома')
+    year = models.PositiveIntegerField(help_text='год')
+    photos = models.ManyToManyField(Photo)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
